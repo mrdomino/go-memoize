8cf1742842a2a387f0032a0fe29eb4e293a351fc
Steven Dee (Jōshin) <mrdomino@stairwell.com>
2025-04-14 05:21:13 -0700

    Example of memoizing a gRPC server

--- examples/grpc/greeter_server/main.go
+++ examples/grpc/greeter_server/main.go
@@ -27,34 +27,61 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 
+	"github.com/bradfitz/gomemcache/memcache"
+	"github.com/mrdomino/go-memoize"
 	"google.golang.org/grpc"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
 var (
 	port = flag.Int("port", 50051, "The server port")
+
+	memcacheAddr = flag.String("memcache_addr", "", "memcache server address")
 )
 
 // server is used to implement helloworld.GreeterServer.
 type server struct {
 	pb.UnimplementedGreeterServer
+	memoSayHello memoize.Func[*pb.HelloRequest, *pb.HelloReply]
+}
+
+func newServer(cache memoize.Cache) *server {
+	s := &server{}
+	s.memoSayHello = memoize.Wrap(cache, func(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+		return s.SayHello_Raw(ctx, in)
+	}, memoize.WithTTL(10*time.Minute), memoize.WithErrorFunc(func(err error) {
+		log.Printf("ERROR: %v", err)
+	}))
+	return s
 }
 
 // SayHello implements helloworld.GreeterServer
-func (s *server) SayHello(_ context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	return s.memoSayHello(ctx, in)
+}
+
+func (s *server) SayHello_Raw(_ context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	log.Printf("Received: %v", in.GetName())
 	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
 }
 
 func main() {
 	flag.Parse()
+	var cache memoize.Cache = memoize.NewLocalCache()
+	if *memcacheAddr != "" {
+		client := memcache.New(*memcacheAddr)
+		if err := client.Ping(); err != nil {
+			log.Fatalf("memcache.Ping(%q): %v", *memcacheAddr, err)
+		}
+	}
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterGreeterServer(s, &server{})
+	pb.RegisterGreeterServer(s, newServer(cache))
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
